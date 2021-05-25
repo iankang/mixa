0e0b6359dc24062c62ecb9d4272e59f5214c515e
iankang <ianngech@gmail.com>
2021-05-25 23:07:02 +0300

    detail page design done

--- lib/utils/ui/home_detail.dart
+++ lib/utils/ui/home_detail.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mixa/utils/models/mix.dart';
+import 'package:seekbar/seekbar.dart';
+import 'dart:async';
+
 import 'package:toast/toast.dart';
 
 class HomeDetail extends StatelessWidget {
@@ -7,10 +10,21 @@ class HomeDetail extends StatelessWidget {
 
   HomeDetail({Key key, this.mix}) : super(key: key);
 
+  double _value = 0.0;
+  double _secondValue = 0.0;
+
+  Timer _progressTimer;
+  Timer _secondProgressTimer;
+
+  bool _done = false;
+
+  @override
+  void initState() {}
+
   @override
   Widget build(BuildContext context) {
     var top = SizedBox(
-      height: 80.0,
+      height: 40.0,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.end,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -23,7 +37,7 @@ class HomeDetail extends StatelessWidget {
           //           image: NetworkImage(mix.albumArt), fit: BoxFit.cover)),
           // ),
           Padding(
-              padding: const EdgeInsets.only(left: 20.0),
+              padding: const EdgeInsets.only(left: 5.0),
               child: InkWell(
                   onTap: () {
                     Navigator.pop(context);
@@ -33,7 +47,7 @@ class HomeDetail extends StatelessWidget {
                     color: Colors.black,
                   ))),
           Padding(
-            padding: const EdgeInsets.only(right: 20.0),
+            padding: const EdgeInsets.only(right: 5.0),
             child: CircleAvatar(
               backgroundImage: NetworkImage(
                 'https://iisy.fi/wp-content/uploads/2018/08/user-profile-male-logo.jpg',
@@ -44,36 +58,145 @@ class HomeDetail extends StatelessWidget {
         ],
       ),
     );
-    var albumArt = SizedBox(
-        height: 200.0,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(
-                      'https://iisy.fi/wp-content/uploads/2018/08/user-profile-male-logo.jpg',
-                    ),
-                    fit: BoxFit.cover,
-                    colorFilter:
-                        ColorFilter.mode(Colors.black26, BlendMode.darken)),
-                color: Colors.grey,
+
+    var albumImage = Expanded(
+        child: Padding(
+      padding: const EdgeInsets.all(64),
+      child: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(mix.albumArt), fit: BoxFit.cover),
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(12)),
+      ),
+    ));
+    var songTitle = Container(
+      padding: EdgeInsets.all(4),
+      child: Column(
+        children: [
+          Text(
+            mix.mixName,
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            mix.dj,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16.0),
+          )
+        ],
+      ),
+    );
+    var seeking = Container(
+      padding: EdgeInsets.symmetric(horizontal: 28, vertical: 8),
+      alignment: Alignment.center,
+      child: SeekBar(
+        value: _value,
+        secondValue: _secondValue,
+        barColor: Colors.blue,
+        progressColor: Colors.blue,
+        secondProgressColor: Colors.blue.withOpacity(0.5),
+        onStartTrackingTouch: () {
+          Toast.show("startTrackingTouch", context,
+              duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
+        },
+        onProgressChanged: (value) {
+          Toast.show('onProgressChanged: $value', context,
+              duration: Toast.LENGTH_SHORT);
+        },
+        onStopTrackingTouch: (value) {
+          Toast.show('onStopTrackingTouch: $value', context,
+              duration: Toast.LENGTH_SHORT);
+        },
+      ),
+    );
+    var organizationOperations = SizedBox(
+      height: 40.0,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 28.0, vertical: 10.0),
+              child: InkWell(
+                  onTap: () {
+                    Toast.show("repeat", context, duration: Toast.LENGTH_SHORT);
+                  },
+                  child: Icon(
+                    Icons.repeat,
+                    color: Colors.black,
+                  ))),
+          Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 28.0, vertical: 10.0),
+              child: InkWell(
+                onTap: () {
+                  Toast.show('playlist', context, duration: Toast.LENGTH_SHORT);
+                },
+                child: Icon(Icons.playlist_add_outlined),
+              )),
+        ],
+      ),
+    );
+    var player = Container(
+      padding: const EdgeInsets.all(24),
+      child: Row(
+        children: [
+          Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: InkWell(
+                  onTap: () {
+                    Toast.show('back', context, duration: Toast.LENGTH_SHORT);
+                  },
+                  child: Icon(
+                    Icons.skip_previous,
+                    size: 24.0,
+                  ),
+                ),
+              )),
+          Container(
+            padding: const EdgeInsets.all(12),
+            child: InkWell(
+              onTap: () {
+                Toast.show('play', context, duration: Toast.LENGTH_SHORT);
+              },
+              child: Icon(
+                Icons.play_arrow,
+                size: 36.0,
               ),
-            )
-          ],
-        ));
-    return Scaffold(
-      body: Column(children: <Widget>[
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [albumArt],
             ),
           ),
-        )
-      ]),
+          Expanded(
+              flex: 2,
+              child: InkWell(
+                  onTap: () {
+                    Toast.show('next', context, duration: Toast.LENGTH_SHORT);
+                  },
+                  child: Icon(
+                    Icons.skip_next,
+                    size: 24.0,
+                  ))),
+        ],
+      ),
+    );
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+        child: Column(
+          children: <Widget>[
+            top,
+            albumImage,
+            songTitle,
+            seeking,
+            organizationOperations,
+            player
+          ],
+        ),
+      ),
     );
   }
 }
